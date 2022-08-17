092803756f0c9a33adaa6af2155dcba553be8a5d
HenryPeeples <hgpeeples48@gmail.com>
2022-08-17 01:39:08 -0500

    Add gorm DB connection

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,25 @@
 package config
+
+// Return DB
+
+import (
+	_ "githhub.com/jinzhu/gorm/dialects/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "henry:Password/bookstore?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
